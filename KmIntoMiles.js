c36a4c39ef104fde4b0d84bd9c7d78f34fb15bcd
Ayush Chaudhary <62565861+Ayush3366@users.noreply.github.com>
2022-12-20 10:40:47 -0500

    Add files via upload

---
+++ KmIntoMiles.js
@@ -0,0 +1,10 @@
+// taking input from the user
+const km = prompt("Enter value in kilometers: ");
+
+// conversion factor
+const convert = 0.621371;
+
+// calculate miles
+const miles = km * convert;
+
+console.log(`${km} km is equal to ${miles} miles.`);
